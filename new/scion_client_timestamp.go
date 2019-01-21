3c94773ca36fdb0eda83eb2463afb3acfb75dec7
manish978973 <manishrchandran@gmail.com>
2019-01-21 15:26:36 +0100

    Update scion_client_timestamp.go

--- new/scion_client_timestamp.go
+++ new/scion_client_timestamp.go
@@ -75,11 +75,11 @@ sendPacketBuffer := make([]byte, 16)   //Intiating a dynamic array of respective
 seed := rand.NewSource(time.Now().UnixNano())
 
 	var total_number int64 = 0
-  iters := 0  // number of iterations
+  iterations := 0  // number of iterations
 	num_tries := 0 //no of attempts
 
 
-  for iters < TOTAL_NUM_ITERS && num_tries < TOTAL_MAX_NUM_TRIES{
+  for iterations < TOTAL_NUM_ITERS && num_tries < TOTAL_MAX_NUM_TRIES{
 
  num_tries= num_tries+1
  id := rand.New(seed).Uint64() //generating random value
@@ -98,21 +98,21 @@ seed := rand.NewSource(time.Now().UnixNano())
        			time_received, _ := binary.Varint(receivePacketBuffer[n:]) //estimating the time received so as to compute Latency
        			diff := (time_received - time_sent.UnixNano())
        			total_number += diff
-       			iters += 1
+       			iterations += 1
        		}
        	}
 
-       	if iters != TOTAL_NUM_ITERS {
+       	if iterations != TOTAL_NUM_ITERS {
        	logerror(fmt.Errorf("Error, exceeded attempts max"))
        	}
 
-       	var difference float64 = float64(total_number) / float64(iters)  //Taking average of latencies for precision
+       	var difference float64 = float64(total_number) / float64(iterations)  //Taking average of latencies for precision
 
        	fmt.Printf("\nClient: %s\nServer: %s\n", clientAddress, serverAddress);
        	fmt.Println("LATENCY_TIMESTAMP_METHOD:")
        	// Print in ms, so divide by 1e6 from nano
-       	fmt.Printf("\tRTT - %.3fms\n", difference/1e9)
-       	fmt.Printf("\tLatency - %.3fms\n", difference/2e9)
+       	fmt.Printf("\tRTT is - %.3fns\n", difference)      //estimating RTT
+       	fmt.Printf("\tLatency is - %.3fns\n", difference/2) //estimating Latency
 
 
 
